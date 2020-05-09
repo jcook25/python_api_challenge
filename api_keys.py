9fc59ce04e36740e3b296762436872c9db15d69b
jcook <57375310+jcook25@users.noreply.github.com>
2020-05-09 04:56:18 -0400

    api key scramble

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "0cde14c8c7462ec293af6b92df1ff44e"
+weather_api_key = "0ffddfccadsfasdfasdfaasdfadsfasddfadfadsfasdfde14c8c7462ec2asdfasdfasdfasdfasdfasdfasdf93af6b92df1ff44e-----asdfasdf"
 
 # Google API Key
-g_key = "AIzaSyBB35PQkd6oVWi_WC1ppCJf_QrYDv2BJFw"
+g_key = "AIasdfasdfazasdfasdfadsfdfffaeSfyeBB35PQkd6oVWi_WC1pasdDDDpCJf_QrYDv2agdfadseadEEEDAFgBJFw"
+
+# changed for privacy, but I do have them and they did work
