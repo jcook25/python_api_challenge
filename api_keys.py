14b38f54609bceceb4bf09b82111e3c5667693b5
jcook <57375310+jcook25@users.noreply.github.com>
2020-05-09 05:08:05 -0400

    map images and api deletion

--- api_keys.py
+++ api_keys.py
@@ -1,7 +1,7 @@
 # OpenWeatherMap API Key
-weather_api_key = "0ffddfccadsfasdfasdfaasdfadsfasddfadfadsfasdfde14c8c7462ec2asdfasdfasdfasdfasdfasdfasdf93af6b92df1ff44e-----asdfasdf"
+weather_api_key = "removed for security"
 
 # Google API Key
-g_key = "AIasdfasdfazasdfasdfadsfdfffaeSfyeBB35PQkd6oVWi_WC1pasdDDDpCJf_QrYDv2agdfadseadEEEDAFgBJFw"
+g_key = "removed for security"
 
-# changed for privacy, but I do have them and they did work
+# changed for security, but I do have them and they did work
